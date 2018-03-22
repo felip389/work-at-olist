06be6b0d94909c2ee82df7e270b2d9805a33830e
Felipe <felipe.destefani@gmail.com>
2018-03-22 18:57:03 -0300

    Heroku config

--- callBilling/settings.py
+++ callBilling/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_m$u!tn*pe3&=%l9feydje34n=g+1t+k8qkc3f%&j5r05vk*!@'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -77,13 +79,9 @@ WSGI_APPLICATION = 'callBilling.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'callbilling',
-        'USER': 'felipe',
-        'PASSWORD': '123456',
-        'HOST': '127.0.0.1',
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
 }
 
 
